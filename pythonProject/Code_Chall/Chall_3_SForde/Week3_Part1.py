c9359dc7101da216277207a70124c7fd1947fa4a
suzanneForde <suzanne_forde@uri.edu>
2024-02-16 01:13:32 -0500

    Part 1 end-ish

--- pythonProject/Code_Chall/Chall_3_SForde/Week3_Part1.py
+++ pythonProject/Code_Chall/Chall_3_SForde/Week3_Part1.py
@@ -4,31 +4,36 @@
 ###
 
 import os
+#
+# file_path0 = r"C:/TEST_FOLDER"
+# os.mkdir(file_path0)
+#
+# file_path1 = r"C:/TEST_FOLDER/draft_code"
+# os.mkdir(file_path1)
+# #
+# file_path2 = r"C:/TEST_FOLDER/includes"
+# os.mkdir(file_path2)
+#
+# file_path3 = r"C:/TEST_FOLDER/layouts"
+# os.mkdir(file_path3)
+#
+# file_path4 = r"C:/TEST_FOLDER/sites"
+# os.mkdir(file_path4)
+#
+#
+# subfolder_names = ['pending', 'complete']
+# for subfolder_name in subfolder_names:
+#     os.makedirs(os.path.join(r"C:/TEST_FOLDER/draft_code", subfolder_name))
+#
+#
+# subfolder_names2 = ['default', 'post']
+# for subfolder_name in subfolder_names2:
+#     os.makedirs(os.path.join(r"C:/TEST_FOLDER/layouts", subfolder_name))
+#
+#
+# subfolder_names3 = ['posted']
+# for subfolder_name in subfolder_names3:
+#     os.makedirs(os.path.join(r"C:/TEST_FOLDER/layouts/post", subfolder_name))
 
-
-file_path1 = r"C:\NRS528_Py_GIS\ArcGIS_Python_Class\pythonProject\Code_Chall\Chall_3_SForde/draft_code"
-os.mkdir(file_path1)
-
-file_path2 = r"C:\NRS528_Py_GIS\ArcGIS_Python_Class\pythonProject\Code_Chall\Chall_3_SForde/includes"
-os.mkdir(file_path2)
-
-file_path3 = r"C:\NRS528_Py_GIS\ArcGIS_Python_Class\pythonProject\Code_Chall\Chall_3_SForde/layouts"
-os.mkdir(file_path3)
-
-file_path4 = r"C:\NRS528_Py_GIS\ArcGIS_Python_Class\pythonProject\Code_Chall\Chall_3_SForde/sites"
-os.mkdir(file_path4)
-
-
-subfolder_names = ['pending', 'complete']
-for subfolder_name in subfolder_names:
-    os.makedirs(os.path.join('draft_code', subfolder_name))
-
-
-subfolder_names2 = ['default', 'post']
-for subfolder_name in subfolder_names2:
-    os.makedirs(os.path.join('layouts', subfolder_name))
-
-
-file_path5 = r"C:\NRS528_Py_GIS\ArcGIS_Python_Class\pythonProject\Code_Chall\Chall_3_SForde/layouts/post/posted"
-os.mkdir(file_path5)
+os.removedirs(r"C:\TEST_FOLDER")
 
