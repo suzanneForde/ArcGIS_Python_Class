5058bca68c9e2a1f8c1c25add5c591289623248a
suzanneForde <suzanne_forde@uri.edu>
2024-02-16 00:36:58 -0500

    part 1 of part 1

--- pythonProject/Code_Chall/Chall_3_SForde/Week3_Part1.py
+++ pythonProject/Code_Chall/Chall_3_SForde/Week3_Part1.py
@@ -3,14 +3,14 @@
 # Part 1
 ###
 
-import os
+# import os
+#
+#
+#
+# subfolder_names = ['pending', 'complete']
+# for subfolder_name in subfolder_names:
+#     os.makedirs(os.path.join('draft_code', subfolder_name))
 
-
-os.makedirs("./draft_code/pending")
-
-complete = "C:\ArcGIS_Python_Class\pythonProject\Code_Chall\Chall_3_SForde/draft_code"
-
-os.makedirs("./complete")
 
 
 
