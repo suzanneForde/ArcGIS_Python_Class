dd3d7eab27882f638c504ae43e8818f1a7aaf6e8
suzanneForde <suzanne_forde@uri.edu>
2024-02-16 01:31:28 -0500

    part 1

--- pythonProject/Code_Chall/Chall_3_SForde/Week3_Part1.py
+++ pythonProject/Code_Chall/Chall_3_SForde/Week3_Part1.py
@@ -4,36 +4,58 @@
 ###
 
 import os
-#
-# file_path0 = r"C:/TEST_FOLDER"
-# os.mkdir(file_path0)
-#
-# file_path1 = r"C:/TEST_FOLDER/draft_code"
-# os.mkdir(file_path1)
-# #
-# file_path2 = r"C:/TEST_FOLDER/includes"
-# os.mkdir(file_path2)
-#
-# file_path3 = r"C:/TEST_FOLDER/layouts"
-# os.mkdir(file_path3)
-#
-# file_path4 = r"C:/TEST_FOLDER/sites"
-# os.mkdir(file_path4)
-#
-#
-# subfolder_names = ['pending', 'complete']
-# for subfolder_name in subfolder_names:
-#     os.makedirs(os.path.join(r"C:/TEST_FOLDER/draft_code", subfolder_name))
-#
-#
-# subfolder_names2 = ['default', 'post']
-# for subfolder_name in subfolder_names2:
-#     os.makedirs(os.path.join(r"C:/TEST_FOLDER/layouts", subfolder_name))
-#
-#
-# subfolder_names3 = ['posted']
-# for subfolder_name in subfolder_names3:
-#     os.makedirs(os.path.join(r"C:/TEST_FOLDER/layouts/post", subfolder_name))
 
-os.removedirs(r"C:\TEST_FOLDER")
+
+file_path0 = r"C:/TEST_FOLDER"
+os.mkdir(file_path0)
+
+
+## Creating subdirectories
+
+file_path1 = r"C:/TEST_FOLDER/draft_code"
+os.mkdir(file_path1)
+#
+file_path2 = r"C:/TEST_FOLDER/includes"
+os.mkdir(file_path2)
+
+file_path3 = r"C:/TEST_FOLDER/layouts"
+os.mkdir(file_path3)
+
+file_path4 = r"C:/TEST_FOLDER/sites"
+os.mkdir(file_path4)
+
+
+## Creating sub-sub directories
+
+subfolder_names = ['pending', 'complete']
+for subfolder_name in subfolder_names:
+    os.makedirs(os.path.join(r"C:/TEST_FOLDER/draft_code", subfolder_name))
+
+
+subfolder_names2 = ['default', 'post']
+for subfolder_name in subfolder_names2:
+    os.makedirs(os.path.join(r"C:/TEST_FOLDER/layouts", subfolder_name))
+
+
+## Sub-sub-sub directories
+
+subfolder_names3 = ['posted']
+for subfolder_name in subfolder_names3:
+    os.makedirs(os.path.join(r"C:/TEST_FOLDER/layouts/post", subfolder_name))
+
+
+# Checking directory
+list = os.listdir(file_path0)
+print(list)
+
+for folder_name in list:
+    if folder_name == "includes":
+        print('name dir EXISTS')
+else:
+    print('name dir NOT EXIST')
+
+# Prints both if and else statements?
+
+
+# os.removedirs(file_path0)
 
