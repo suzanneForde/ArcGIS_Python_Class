6c8a3db74c69b541e274119a062ea353efc11aad
suzanneForde <suzanne_forde@uri.edu>
2024-02-16 00:46:55 -0500

    part 1 subfolders

--- pythonProject/Code_Chall/Chall_3_SForde/Week3_Part1.py
+++ pythonProject/Code_Chall/Chall_3_SForde/Week3_Part1.py
@@ -3,15 +3,28 @@
 # Part 1
 ###
 
-# import os
-#
-#
-#
-# subfolder_names = ['pending', 'complete']
-# for subfolder_name in subfolder_names:
-#     os.makedirs(os.path.join('draft_code', subfolder_name))
+import os
 
 
+file_path1 = r"C:\NRS528_Py_GIS\ArcGIS_Python_Class\pythonProject\Code_Chall\Chall_3_SForde/draft_code"
+os.mkdir(file_path1)
+
+file_path2 = r"C:\NRS528_Py_GIS\ArcGIS_Python_Class\pythonProject\Code_Chall\Chall_3_SForde/includes"
+os.mkdir(file_path2)
+
+file_path3 = r"C:\NRS528_Py_GIS\ArcGIS_Python_Class\pythonProject\Code_Chall\Chall_3_SForde/layouts"
+os.mkdir(file_path3)
+
+file_path4 = r"C:\NRS528_Py_GIS\ArcGIS_Python_Class\pythonProject\Code_Chall\Chall_3_SForde/sites"
+os.mkdir(file_path4)
 
 
+subfolder_names = ['pending', 'complete']
+for subfolder_name in subfolder_names:
+    os.makedirs(os.path.join('draft_code', subfolder_name))
+
+
+subfolder_names = ['pending', 'complete']
+for subfolder_name in subfolder_names:
+    os.makedirs(os.path.join('draft_code', subfolder_name))
 
