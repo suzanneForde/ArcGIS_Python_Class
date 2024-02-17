d6610aeb5194efeeace97b69ea90aa639d8d620a
suzanneForde <suzanne_forde@uri.edu>
2024-02-17 11:27:07 -0500

    Suffering is almost over

--- pythonProject/Code_Chall/Chall_3_SForde/Week3_Part3.py
+++ pythonProject/Code_Chall/Chall_3_SForde/Week3_Part3.py
@@ -10,42 +10,43 @@
 import csv
 import datetime
 
-# year_total = {}
-# year_count = {}
-# annual_averages = {}
-# def calculate_annual_average(data):
-#     for row in data:
-#         date_parts = row[0].split('/')
-#         if len(date_parts) < 3:
-#             print("Invalid date format:", row[0])
-#             continue
-#
-#         year = date_parts[2]
-#         value = float(row[1])
-#
-#         if year in year_total:
-#             year_total[year].append(value)
-#             year_count[year] += 1
-#         else:
-#             year_total[year] = [value]
-#             year_count[year] = 1
-#
-#
-#     for year in year_total:
-#         annual_averages[year] = sum(year_total[year]) / year_count[year]
-#         return annual_averages
-#
-# data = []
-# with open('co2_ML.csv', 'r') as file:
-#     csv_reader = csv.reader(file)
-#     next(csv_reader)
-#     for row in csv_reader:
-#         data.append(row)
-#
-# annual_averages = calculate_annual_average(data)
-#
-# for year, average in annual_averages.items():
-#     print(f'Year {year}: {average}')
+year_total = {}
+year_count = {}
+annual_averages = {}
+def calculate_annual_average(data):
+    for row in data:
+        date_parts = row[0].split('/')
+        if len(date_parts) < 3:
+            print("Invalid date format:", row[0])
+            continue
+
+        year = date_parts[2]
+        value = float(row[1])
+
+        if year in year_total:
+            year_total[year].append(value)
+            year_count[year] += 1
+        else:
+            year_total[year] = [value]
+            year_count[year] = 1
+
+
+    for year in year_total:
+        annual_averages[year] = sum(year_total[year]) / year_count[year]
+
+    return annual_averages
+
+data = []
+with open('co2_ML.csv', 'r') as file:
+    csv_reader = csv.reader(file)
+    next(csv_reader)
+    for row in csv_reader:
+        data.append(row)
+
+annual_averages = calculate_annual_average(data)
+
+for year, average in annual_averages.items():
+    print(f'Year {year}: {average}')
 
 
 
@@ -87,9 +88,90 @@ print(f"Average: {average}")
 
 ## 3. Seasonal average if Spring (March, April, May), Summer (June, July, August),
 ## Autumn (September, October, November) and Winter (December, January, February).
+def calculate_seasonal_average(data):
+    seasonal_totals = {'Spring': 0, 'Summer': 0, 'Autumn': 0, 'Winter': 0}
+    seasonal_counts = {'Spring': 0, 'Summer': 0, 'Autumn': 0, 'Winter': 0}
+    def get_season(month):
+        if 3 <= month <= 5:
+            return 'Spring'
+        elif 6 <= month <= 8:
+            return 'Summer'
+        elif 9 <= month <= 11:
+            return 'Autumn'
+        else:
+            return 'Winter'
 
+    for row in data:
+        date_parts = row[0].split('/')
+        if len(date_parts) < 3:
+            print("Invalid date format:", row[0])
+            continue
 
+        month = int(date_parts[1])
 
+        season = get_season(month)
+
+        value = float(row[1])
+        seasonal_totals[season] += value
+        seasonal_counts[season] += 1
+
+    seasonal_averages = {}
+    for season in seasonal_totals:
+        if seasonal_counts[season] > 0:
+            seasonal_averages[season] = seasonal_totals[season] / seasonal_counts[season]
+        else:
+            seasonal_averages[season] = 0
+
+    return seasonal_averages
+
+data = []
+with open('co2_ML.csv', 'r') as file:
+    csv_reader = csv.reader(file)
+    next(csv_reader)
+    for row in csv_reader:
+        data.append(row)
+
+seasonal_averages = calculate_seasonal_average(data)
+
+for season, average in seasonal_averages.items():
+    print(f"{season} Average: {average}")
 
 ## 4. Calculate the anomaly for each value in the dataset relative to the mean for the entire time series.
+def calculate_mean(data):
+    total_sum = 0
+    total_count = 0
+    for row in data:
+        total_sum += float(row[1])
+        total_count += 1
+    if total_count == 0:
+        return None
+    return total_sum / total_count
 
+
+def calculate_anomalies(data):
+    mean = calculate_mean(data)
+    if mean is None:
+        print("Error: Cannot calculate anomaly, dataset is empty.")
+        return None
+
+    anomalies = []
+    for row in data:
+        value = float(row[1])
+        anomaly = value - mean
+        anomalies.append(anomaly)
+
+    return anomalies
+
+
+data = []
+with open('co2_ML.csv', 'r') as file:
+    csv_reader = csv.reader(file)
+    next(csv_reader)
+    for row in csv_reader:
+        data.append(row)
+
+anomalies = calculate_anomalies(data)
+
+if anomalies is not None:
+    for i, anomaly in enumerate(anomalies):
+        print(f"Anomaly for value {i + 1}: {anomaly}")
