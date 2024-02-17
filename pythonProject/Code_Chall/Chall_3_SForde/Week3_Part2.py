29495c2e23c80d7464bcee036c8dee172e5c86e2
suzanneForde <suzanne_forde@uri.edu>
2024-02-16 20:43:26 -0500

    struggle 8

--- pythonProject/Code_Chall/Chall_3_SForde/Week3_Part2.py
+++ pythonProject/Code_Chall/Chall_3_SForde/Week3_Part2.py
@@ -5,9 +5,11 @@
 
 import pickle
 Turtle = open('IDK.dat', 'wb')
-data = 
+data = input('Enter data: ')
 
-
+pickle.dump(data, Turtle)
+Turtle.close()
+print("Data recorded...")
 
 # sys.stderr.write('Pickle')
 
