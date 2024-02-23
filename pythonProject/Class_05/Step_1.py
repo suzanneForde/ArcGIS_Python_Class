da68fb900cc35ff7affaf489b7fec99b38804bf4
suzanneForde <suzanne_forde@uri.edu>
2024-02-23 14:46:22 -0500

    step 1 task 1

--- pythonProject/Class_05/Step_1.py
+++ pythonProject/Class_05/Step_1.py
@@ -41,6 +41,8 @@ import arcpy
 import arcpy
 arcpy.env.workspace = r"C:\NRS528_Py_GIS\ArcGIS_Python_Class\pythonProject\Class_05"
 
+arcpy.env.overwriteOutput = True
+
 in_Table = r"Step_1_Deep_Coral.csv"
 x_coords = "decimalLongitude"
 y_coords = "decimalLatitude"
@@ -51,18 +53,21 @@ saved_Layer = r"Step_1_Deep_Coral_Output.shp"
 spRef = arcpy.SpatialReference(4326)  # 4326 == WGS 1984
 
 lyr = arcpy.MakeXYEventLayer_management(in_Table, x_coords, y_coords, out_Layer, spRef, z_coords)
-
-##### 2. Print the count of the number of records in the file. (Hint: see above!)
-# https://pro.arcgis.com/en/pro-app/latest/tool-reference/data-management/get-count.htm
-
+#
+# ##### 2. Print the count of the number of records in the file. (Hint: see above!)
+# # https://pro.arcgis.com/en/pro-app/latest/tool-reference/data-management/get-count.htm
+#
 print(arcpy.GetCount_management(out_Layer))
-
-##### 3. Check the correct coordinate system has been applied (Hint: see last week!)
-
+#
+# ##### 3. Check the correct coordinate system has been applied (Hint: see last week!)
+#
 arcpy.CopyFeatures_management(lyr, saved_Layer)
-
+#
 if arcpy.Exists(saved_Layer):
     print("Created file successfully!")
+
+desc = arcpy.Describe(saved_Layer)
+print(desc.spatialReference.name)
 
 ##### 4. Visualize the file in ArcPro by dragging it into the program.
 
