edefad495af9de5728c8eafd3be8231cde109e47
suzanneForde <suzanne_forde@uri.edu>
2024-03-15 18:00:45 -0400

    file management

---
+++ pythonProject/Code_Chall/Midterm_Tool_SForde/Midterm_Tool_SForde.py
@@ -0,0 +1,11 @@
+
+
+import arcpy
+import os
+
+# Setting up the workspace and file paths
+workspace = arcpy.env.workspace = r'C:\NRS528_Py_GIS\ArcGIS_Python_Class\pythonProject\Code_Chall\Midterm_Tool_SForde\School_Library_Data'
+School_shapefile = "FACILITY_Schools_pk12_2023.shp"
+Library_shapefile = "FACILITY_Public_Libraries_2021.shp"
+
+
