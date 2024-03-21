545665938875958e4df0b42c4f9430fee8e0845c
suzanneForde <suzanne_forde@uri.edu>
2024-03-21 03:00:01 -0400

    Class_07 catch-up

--- pythonProject/Class_07/Step_1.py
+++ pythonProject/Class_07/Step_1.py
@@ -20,8 +20,8 @@ os.chdir("../")
 print(glob.glob("*")) # no pattern match, lists all folders and files
 # If you changed directory, you may need to change it back:
 
-
 # Part 2 - List files using os (more painful)
+os.chdir("Class_07")
 all = os.listdir(os.curdir)# files and directories
 print(all)
 files = list(filter(os.path.isfile, os.listdir(os.curdir)))  # files only, might not find anything
@@ -50,18 +50,35 @@ print(arcpy.ListRasters("*", "TIF"))
 # 1 - Using the supplied data in Step_1.zip (extract to a folder named Step_1), do the following:
 # Hint, you should change your directory to Step_1
 import arcpy
-os.chdir("CHANGE TO YOUR DIR")
-arcpy.env.workspace = "CHANGE TO YOUR DIR"
+os.chdir(r"C:\NRS528_Py_GIS\ArcGIS_Python_Class\pythonProject\Class_07\Step_1")
+arcpy.env.workspace = r"C:\NRS528_Py_GIS\ArcGIS_Python_Class\pythonProject\Class_07\Step_1"
 
 # a - List and count all shapefiles, how many are there?
+shape_files = arcpy.arcpy.ListFiles("*.shp")
+print(len(arcpy.ListFiles("*.shp")))
 
 # b - List and count all csv, how many are there?
+shape_files = arcpy.arcpy.ListFiles("*.shp")
+print(len(arcpy.ListFiles("*.shp")))
 
 # c - List and count all folders, how many are there?
+list_all_folders = os.listdir(os.curdir)
+list_all_folders = [x for x in list_all_folders if os.path.isdir(x)]
+print(list_all_folders)
+print(len(list_all_folders))
 
 # d - List and count all rasters in GRID format, how many are there?
+rasters_grid = arcpy.ListRasters("*", "GRID")
+print(rasters_grid)
+print(len(rasters_grid))
 
 # e - List and count all rasters in TIF format, how many are there?
+rasters_tif = arcpy.ListRasters("*", "TIF")
+print(rasters_tif)
+print(len(rasters_tif))
 
 # f - List and count all folders beginning with the letter/character S_, how many are there?
-
+list_s_folders = os.listdir(os.curdir)
+list_s_folders = [x for x in list_s_folders if "S_" in x]
+print(list_s_folders)
+print(len(list_s_folders))
