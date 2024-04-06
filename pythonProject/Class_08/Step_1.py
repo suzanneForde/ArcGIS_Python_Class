3a9032d11574b67d0fe0c667c90be0daa7cde73f
suzanneForde <suzanne_forde@uri.edu>
2024-04-06 00:05:32 -0400

    step 1 class 8

--- pythonProject/Class_08/Step_1.py
+++ pythonProject/Class_08/Step_1.py
@@ -14,15 +14,15 @@
 # and provide the arguments, see the example below.
 
 
-def do_math(x,y):
-    """This is docstring, it can be used to explain what the function does."""
-    value = x + y
-    return value
-
-
-print(do_math(1, 2))
-print(do_math(10, 10))
-print(do_math(20, 50))
+# def do_math(x,y):
+#     """This is docstring, it can be used to explain what the function does."""
+#     value = x + y
+#     return value
+#
+#
+# print(do_math(1, 2))
+# print(do_math(10, 10))
+# print(do_math(20, 50))
 
 # Task 1 - Comment out the above code, and then uncomment and execute the below code, analyze the error,
 # why did the code below not work?
@@ -35,13 +35,26 @@ print(do_math(20, 50))
 # print_me()
 
 # Task 2 - Repair the code in the area below (Hint: do you need to edit the function or not?):
-
+# def print_me(str):
+#     """This prints a passed string into this function"""
+#     print(str)
+#     return
+#
+# print_me("Hello!")
 
 # Task 3 - Turn the code below into a function, and then run the code using the inputs: a and b (shown), and
-# # i = s and j = e
-#
+# i = s and j = e
+
 # i = "a"
 # j = "b"
 #
 # output = i + j + j + i
 # print(output)
+#
+# def word_maker(i, j):
+#     output = i + j + j + i
+#     print(output)
+#     return
+#
+# word_maker("a", "b")
+# word_maker("s", "e")
