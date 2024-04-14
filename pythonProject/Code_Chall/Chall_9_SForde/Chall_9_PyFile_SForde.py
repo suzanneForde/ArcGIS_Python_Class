292128f5eaa030ab92d79a9135e25fb903a3b595
suzanneForde <suzanne_forde@uri.edu>
2024-04-14 03:54:52 -0400

    Chall 9

--- pythonProject/Code_Chall/Chall_9_SForde/Chall_9_PyFile_SForde.py
+++ pythonProject/Code_Chall/Chall_9_SForde/Chall_9_PyFile_SForde.py
@@ -1,3 +1,16 @@
 
+####
+# Coding Challenge 9
+####
+
+# Objective: utilize arcpy.da module to undertake some basic partitioning of dataset.
+# Work with the Forest Health Works dataset from RI GIS (provided as a downloadable ZIP file in this repository).
+# Using the arcpy.da module, extract all sites that have a photo of the invasive species (Field: PHOTO)
+# into a new Shapefile, and do some basic counts of the dataset.
+# In summary, addressing the following:
+# 1. Count how many individual records have photos, and how many do not (2 numbers), print the results.
+# 2. Count how many unique species there are in the dataset, print the result.
+# 3. Generate two shapefiles, one with photos and the other without.
+
 
 
